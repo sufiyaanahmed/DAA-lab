63b09d7f2806ae59bc4d92df12fd19fcb1e2a1f7
sufiyaanahmed <139116803+sufiyaanahmed@users.noreply.github.com>
2023-09-01 09:47:36 +0530

    Update allpairsusingfloyd.cpp

--- allpairsusingfloyd.cpp
+++ allpairsusingfloyd.cpp
@@ -1,56 +1,47 @@
-#include&lt;stdio.h&gt;
-#include&lt;conio.h&gt;
-int a[10][10],n;
+#include <stdio.h>
+
+int a[10][10], n;
+
 void floyds();
-int min(int,int);
-void main()
-{
-int i,j;
-clrscr();
-printf(&quot;\nenter the no. of vertices:\t&quot;);
-scanf(&quot;%d&quot;,&amp;n);
-printf(&quot;\nenter the cost matrix:\n&quot;);
-for(i=1;i&lt;=n;i++)
-{
-for(j=1;j&lt;=n;j++)
-{
-scanf(&quot;%d&quot;,&amp;a[i][j]);
+int min(int, int);
+
+int main() {
+    int i, j;
+    
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+    
+    printf("Enter the cost matrix:\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    
+    floyds();
+    
+    return 0;
 }
+
+void floyds() {
+    int i, j, k;
+    for (k = 1; k <= n; k++) {
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n; j++) {
+                a[i][j] = min(a[i][j], a[i][k] + a[k][j]);
+            }
+        }
+    }
+    
+    printf("\nAll pair shortest path matrix is:\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n\n");
+    }
 }
-floyds();
-getch();
-}
-void floyds()
-{
-int i,j,k;
-for(k=1;k&lt;=n;k++)
-{
-for(i=1;i&lt;=n;i++)
-{
-for(j=1;j&lt;=n;j++)
-{
-a[i][j]=min(a[i][j],a[i][k]+a[k][j]);
-}
-}
-}
-printf(&quot;\nall pair shortest path matrix is:\n&quot;);
-for(i=1;i&lt;=n;i++)
-{
-for(j=1;j&lt;=n;j++)
-{
-printf(&quot;%d\t&quot;,a[i][j]);
-}
-printf(&quot;\n\n&quot;);
-}
-}
-int min(int x,int y)
-{
-if(x&lt;y)
-{
-return x;
-}
-else
-{
-return y;
-}
+
+int min(int x, int y) {
+    return (x < y) ? x : y;
 }
